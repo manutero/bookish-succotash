643806de75e6d5dddd3a0aed16bd344f1af38efe
Manu <martero@tuenti.com>
2018-03-01 16:34:18 +0100

    printing a red square

--- autocomplete.js
+++ autocomplete.js
@@ -1,13 +1,39 @@
 const foo = () => {
   console.log("HEY!");
+  const div = elementCreator.getDiv("a", "b", "c");
+  document.activeElement.parentNode.appendChild(div);
 };
 
 const autoCompletionDetector = AutoCompletionDetector(foo);
+const elementCreator = ElementCreator();
 
 window.onkeydown = e => {
   var code = e.keyCode ? e.keyCode : e.which;
   autoCompletionDetector.read(code);
 };
+
+function ElementCreator() {
+  const htmlToElement = html => {
+    var template = document.createElement("template");
+    html = html.trim();
+    template.innerHTML = html;
+    return template.content.firstChild;
+  };
+  return {
+    getDiv: (...items) => {
+      console.log(items);
+      const element = htmlToElement(
+        "<div><ul>" +
+          items.reduce((acc, item) => acc + `<li>${item}</li>`, "") +
+          "</ul></div>"
+      );
+      element.style.position = "absolute";
+      element.style.zIndex = 999;
+      element.style.background = "red";
+      return element;
+    }
+  };
+}
 
 function AutoCompletionDetector(cb) {
   let expectingSpace = false;
