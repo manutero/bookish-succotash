bc83d65462f80c14068cea6d9bedb91fcb410fa4
Manu <martero@tuenti.com>
2018-03-02 11:06:36 +0100

    using a set to save all words

--- words-collector.js
+++ words-collector.js
@@ -1,29 +1,28 @@
 function WordsCollector() {
-  const words = new Set();
+  const set = new Set();
+
+  const isValid = str => str.length > 3 && /^[a-zA-Z]+$/.test(str);
+
+  const elementHasNoChildrenAndNotEmpty = element =>
+    element.children.length === 0 &&
+    element.textContent.replace(/ |\n/g, "") !== "";
 
   const allTexts = () => {
-    const array = [];
     const elements = document.body.getElementsByTagName("*");
     for (const current of elements) {
-      // Check the element has no children && that it is not empty
-      if (
-        current.children.length === 0 &&
-        current.textContent.replace(/ |\n/g, "") !== ""
-      ) {
-        const words = current.textContent.split(" ");
-        console.log(words);
-        // array.push(current.textContent);
+      if (elementHasNoChildrenAndNotEmpty(current)) {
+        current.textContent.split(" ").forEach(str => {
+          if (isValid(str)) {
+            set.add(str);
+          }
+        });
       }
     }
-    return [
-      "createListElements",
-      "fooExtraLarge",
-      "interactorExtractorFactory",
-      "myElement"
-    ];
+    console.log(set);
+    return Array.from(set);
   };
 
   return {
-    getCurrentWords: (max = 10) => allTexts()
+    getCurrentWords: (n = 5) => allTexts().slice(0, n)
   };
 }
